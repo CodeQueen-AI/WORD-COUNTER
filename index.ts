6c35f1b4cc5cc78f3b40196f3f2716b5f7e4870a
Sumbal Naz <Codeq209@gmail.com>
2024-09-12 16:23:56 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer"
+
+const answers : {
+    Sentence : string
+} = await inquirer.prompt([
+    {
+        name : 'Sentence',
+        type : 'input',
+        message : 'Enter your sentence to count the word:',
+    }
+])
+const words = answers.Sentence.trim().split("")
+console.log(words);
+
+console.log(`Your Sentence word count is ${words.length}`);
+
+
